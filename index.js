b74e21f9ab0bb7ddab6bda669175a4d68ca3a0a4
Ting Chung <tchung682@gmail.com>
2020-05-14 15:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function increaseRankBy(n){
   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   var currentnumber;
   for (var i = 0; i < lis.length; i++) {
+    console.log(lis[i].innerHTML);
     currentnumber = parseInt(lis[i].innerHTML) + n
 
     lis[i].innerHTML = currentnumber;
