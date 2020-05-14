e5d8ed91bbc24e40ac969acbefda6b5f378267eb
Ting Chung <tchung682@gmail.com>
2020-05-14 15:33:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
 
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+function increaseRankBy(n){
+  debugger
+  var lis = document.getElementById('app').querySelectorAll('.ranked-list')
+
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt((i + n).toString());
+  }
+
+}
+function deepestChild(){
+  return document.querySelectorAll('#grand-node')
+}
