244e97a33edd8a7eed1b78bd40fa6cb2f61fa293
Ting Chung <tchung682@gmail.com>
2020-05-14 18:56:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function deepestChild(){
   debugger
   var lis = document.getElementById('app').querySelector('div#grand-node')
   var lisChild;
-  while(lis.tagName === "DIV" && lis.tagName != null){
+  while(lis.tagName === "DIV" || lis.tagName != null){
 
       lisChild = lis
       lis = lis.querySelector('div')
